c23f01c23bb384a7b0f450ef68a7af05809be62e
Carlos Pereira <carlosa.r.pereiraneto@gmail.com>
2017-06-04 22:59:39 -0300

    Primeira parte

--- Proportions/ViewController.swift
+++ Proportions/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultOne: UILabel!
+    @IBOutlet weak var unitOne: UITextField!
+    @IBOutlet weak var valueOne: UITextField!
+    
+    @IBAction func calcular(_ sender: Any)
+    {
+        let valueOneFloat = Float(valueOne.text!.replacingOccurrences(of: ",", with: "."))
+        let unitOneFloat = Float(unitOne.text!)
+        let resultOneFloat = ((valueOneFloat!/unitOneFloat!)*1000)
+        resultOne.text = String(resultOneFloat)
+    }
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
